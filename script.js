44049d4046f8d494dfe2e0b471d6d51c7afaf2a2
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2024-11-09 20:29:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let fahrenheitInput = document.getElementById('fahrenheit'),
+celsiusInput = document.getElementById('celsius'),
+kelvinInput = document.getElementById('kelvin');
+
+fahrenheitInput.addEventListener('input', function(){
+    let fahrenheit = parseFloat(fahrenheitInput.value),
+    celsius = (fahrenheit - 32) * (5 / 9),
+    kelvin = celsius + 273.15;
+    celsiusInput.value = celsius;
+    kelvinInput.value = kelvin;
+});
+
+celsiusInput.addEventListener('input', function(){
+    let celsius = parseFloat(celsiusInput.value),
+    fahrenheit = celsius * (9 / 5) + 32,
+    kelvin = celsius + 273.15;
+    fahrenheitInput.value = fahrenheit;
+    kelvinInput.value = kelvin;
+});
+
+kelvinInput.addEventListener('input', function(){
+    let kelvin = parseFloat(kelvinInput.value),
+    celsius = kelvin - 273.15;
+    fahrenheit = celsius * (9 / 5) + 32,
+    fahrenheitInput.value = fahrenheit;
+    celsiusInput.value = celsius;
+});
